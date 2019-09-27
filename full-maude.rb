a93bd0a83488613c800683d98f7f2cd85fd634c4
FengZiGG <520282483@qq.com>
2019-09-27 14:55:14 +0800

    Update full-maude.rb

--- full-maude.rb
+++ full-maude.rb
@@ -1,6 +1,6 @@
 class FullMaude < Formula
   homepage "http://maude.cs.illinois.edu"
-  version "2.7"
+  version "2.7.1"
   url "http://maude.cs.illinois.edu/w/images/c/ce/Full-maude-27.zip"
   sha256 "965ea5410224e00846e0e6a2872edd861fe580ede9507a462057ed0300919396"
   depends_on "maude"
